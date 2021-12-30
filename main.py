d7838dd937e384939c0485435f70e64b77732e8b
AmatorAVG <amatoravg@gmail.com>
2021-12-30 18:07:09 +0700

    Add task link

--- main.py
+++ main.py
@@ -40,7 +40,20 @@ while True:
         elif parsed_response['status'] == "found":
             timestamp_to_request = parsed_response['last_attempt_timestamp']
             logger.debug(f"timestamp_to_request: {timestamp_to_request}")
-            bot.send_message(chat_id=582843947, text="Преподаватель проверил работу!")
+
+            # {'last_attempt_timestamp': 1640860287.560001,
+            #  'new_attempts': [{'is_negative': True,
+            #                    'lesson_title': 'Деплой по-взрослому',
+            #                    'lesson_url': 'https://dvmn.org/modules/django/lesson/deploy-hard-way/',
+            #                    'submitted_at': '2021-12-30T13:31:27.560001+03:00',
+            #                    'timestamp': 1640860287.560001}],
+            #  'request_query': [['timestamp', '1640860278.832847']],
+            #  'status': 'found'}
+            for attempt in parsed_response['new_attempts']:
+                result = 'К сожалению, в работе нашлись ошибки.' if attempt['is_negative'] else 'Преподавателю всё понравилось, можно приступать к следующему уроку!'
+                bot.send_message(chat_id=582843947,
+                                 text=f"""У вас проверили работу \"<a href="{attempt['lesson_url']}">{attempt['lesson_title']}</a>\".\n\n{result}""",
+                                 parse_mode=telegram.ParseMode.HTML)
 
     except requests.exceptions.ReadTimeout:
         logger.info("Превышено время ожидания. Отправляем запрос заново...")
