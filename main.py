e4f96b1a99cc69374df2e35e36ccb2d9470145ea
AmatorAVG <amatoravg@gmail.com>
2022-01-06 14:59:15 +0700

    Add f-string, dedent, rename variable

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import textwrap
 import requests
 import time
 from environs import Env
@@ -21,7 +22,7 @@ def main():
 
     url = 'https://dvmn.org/api/long_polling/'
     headers = {
-        "Authorization": "Token " + env('DEVMAN_ACCESS_TOKEN')
+        "Authorization": f"Token {env('DEVMAN_ACCESS_TOKEN')}"
     }
 
     timestamp_to_request = time.time()
@@ -33,19 +34,25 @@ def main():
             response = requests.get(url, headers=headers, timeout=30, params=params)
             response.raise_for_status()
 
-            parsed_response = response.json()
-            if parsed_response['status'] == "timeout":
-                timestamp_to_request = parsed_response['timestamp_to_request']
+            work_checks = response.json()
+            if work_checks['status'] == "timeout":
+                timestamp_to_request = work_checks['timestamp_to_request']
                 logger.debug(f"timestamp_to_request: {timestamp_to_request}")
-            elif parsed_response['status'] == "found":
-                timestamp_to_request = parsed_response['last_attempt_timestamp']
+            elif work_checks['status'] == "found":
+                timestamp_to_request = work_checks['last_attempt_timestamp']
                 logger.debug(f"timestamp_to_request: {timestamp_to_request}")
 
-                for attempt in parsed_response['new_attempts']:
-                    result = 'К сожалению, в работе нашлись ошибки.' if attempt[
-                        'is_negative'] else 'Преподавателю всё понравилось, можно приступать к следующему уроку!'
+                for attempt in work_checks['new_attempts']:
+                    result = (
+                        'К сожалению, в работе нашлись ошибки.'
+                        if attempt['is_negative']
+                        else 'Преподавателю всё понравилось, можно приступать к следующему уроку!'
+                    )
                     bot.send_message(chat_id=chat_id,
-                                     text=f"""У вас проверили работу \"<a href="{attempt['lesson_url']}">{attempt['lesson_title']}</a>\".\n\n{result}""",
+                                     text=textwrap.dedent(f"""\
+                                     У вас проверили работу \"<a href="{attempt['lesson_url']}">{attempt['lesson_title']}</a>\".
+                                     
+                                     {result}"""),
                                      parse_mode=telegram.ParseMode.HTML)
 
         except requests.exceptions.ReadTimeout:
@@ -53,6 +60,7 @@ def main():
         except requests.exceptions.ConnectionError:
             logger.info("Интернет отключится! Отправляем запрос заново через 5 секунд...")
             time.sleep(5)
+
 
 if __name__ == '__main__':
     main()
