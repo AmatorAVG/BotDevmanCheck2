0d690c4f446c3b54fec87982fc2a0fe1dfb61417
AmatorAVG <amatoravg@gmail.com>
2022-01-26 16:01:09 +0700

    Catch errors and write to log

--- main.py
+++ main.py
@@ -33,48 +33,53 @@ def main():
 
     logger.addHandler(TelegramLogsHandler(bot, chat_id))
 
-    logger.debug(bot.get_me())
-    logger.warning("Бот запущен")
-
-    url = 'https://dvmn.org/api/long_polling/'
-    headers = {
-        "Authorization": f"Token {env('DEVMAN_ACCESS_TOKEN')}"
-    }
-
-    timestamp_to_request = time.time()
-
     while True:
         try:
-            params = {"timestamp": timestamp_to_request}
+            logger.debug(bot.get_me())
+            logger.warning("Бот запущен")
 
-            response = requests.get(url, headers=headers, timeout=30, params=params)
-            response.raise_for_status()
+            url = 'https://dvmn.org/api/long_polling/'
+            headers = {
+                "Authorization": f"Token {env('DEVMAN_ACCESS_TOKEN')}"
+            }
 
-            work_checks = response.json()
-            if work_checks['status'] == "timeout":
-                timestamp_to_request = work_checks['timestamp_to_request']
-                logger.debug(f"timestamp_to_request: {timestamp_to_request}")
-            elif work_checks['status'] == "found":
-                timestamp_to_request = work_checks['last_attempt_timestamp']
-                logger.debug(f"timestamp_to_request: {timestamp_to_request}")
+            timestamp_to_request = time.time()
+            while True:
+                try:
+                    params = {"timestamp": timestamp_to_request}
 
-                for attempt in work_checks['new_attempts']:
-                    result = (
-                        'К сожалению, в работе нашлись ошибки.'
-                        if attempt['is_negative']
-                        else 'Преподавателю всё понравилось, можно приступать к следующему уроку!'
-                    )
-                    bot.send_message(chat_id=chat_id,
-                                     text=textwrap.dedent(f"""\
-                                     У вас проверили работу \"<a href="{attempt['lesson_url']}">{attempt['lesson_title']}</a>\".
-                                     
-                                     {result}"""),
-                                     parse_mode=telegram.ParseMode.HTML)
+                    response = requests.get(url, headers=headers, timeout=30, params=params)
+                    response.raise_for_status()
 
-        except requests.exceptions.ReadTimeout:
-            logger.warning("Превышено время ожидания. Отправляем запрос заново...")
-        except requests.exceptions.ConnectionError:
-            logger.warning("Интернет отключится! Отправляем запрос заново через 5 секунд...")
+                    work_checks = response.json()
+                    if work_checks['status'] == "timeout":
+                        timestamp_to_request = work_checks['timestamp_to_request']
+                        logger.debug(f"timestamp_to_request: {timestamp_to_request}")
+                    elif work_checks['status'] == "found":
+                        timestamp_to_request = work_checks['last_attempt_timestamp']
+                        logger.debug(f"timestamp_to_request: {timestamp_to_request}")
+
+                        for attempt in work_checks['new_attempts']:
+                            result = (
+                                'К сожалению, в работе нашлись ошибки.'
+                                if attempt['is_negative']
+                                else 'Преподавателю всё понравилось, можно приступать к следующему уроку!'
+                            )
+                            bot.send_message(chat_id=chat_id,
+                                             text=textwrap.dedent(f"""\
+                                             У вас проверили работу \"<a href="{attempt['lesson_url']}">{attempt['lesson_title']}</a>\".
+                                             
+                                             {result}"""),
+                                             parse_mode=telegram.ParseMode.HTML)
+
+                except requests.exceptions.ReadTimeout:
+                    logger.warning("Превышено время ожидания. Отправляем запрос заново...")
+                except requests.exceptions.ConnectionError:
+                    logger.warning("Интернет отключится! Отправляем запрос заново через 5 секунд...")
+                    time.sleep(5)
+        except Exception as exception:
+            logger.warning("Бот упал с ошибкой:>")
+            logger.warning(exception)
             time.sleep(5)
 
 
